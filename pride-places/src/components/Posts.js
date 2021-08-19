da526cf2c4e84ef93110ce837433e7c87fb543c2
KeslieBox <kesliebox@gmail.com>
2021-08-19 17:31:04 -0500

    updates comments

--- pride-places/src/components/Posts.js
+++ pride-places/src/components/Posts.js
@@ -13,14 +13,14 @@ export default function Posts() {
     const openModal = async(userId, post) => {
         const resp = await fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
         const user = await resp.json()
-        // set current user and post after author of current post is fetched
+        // sets current user and post after author of current post is fetched
         setUser(user)
         setPost(post)
         // setShowModal to true and open modal
         setShowModal(prev => !prev)    
     }
 
-    // fetch all posts on ComponentDidMount and setPosts to response object from fetch Promise
+    // fetches all posts on ComponentDidMount and setPosts to response object from fetch Promise
     useEffect(() => {
         (async() => {
             const resp = await fetch('https://jsonplaceholder.typicode.com/posts')
@@ -33,7 +33,7 @@ export default function Posts() {
         <>
             <div classeName={classes.container}>
                 <ul id='posts-ul'>
-                    {/* iterate through all posts and display as li */}
+                    {/* iterates through all posts and display as li */}
                     {posts && posts.map((post, i) => {
                         const capitalizedTitle = post.title.charAt(0).toUpperCase() + post.title.slice(1)
                         return (
@@ -44,7 +44,7 @@ export default function Posts() {
                     })}
                 </ul>
             </div>
-            {/* display modal content if showModal is set to true */}
+            {/* displays modal content if showModal is set to true */}
             <Modal showModal={showModal} setShowModal={setShowModal} user={user} post={post} title={post.title}/>
         </>
     )  
